2156c768443bca7bea0b9756c13180d7835fb8db
Anna Jordanous <annajordanous@hotmail.com>
2023-01-01 17:04:10 +0000

    saving progress justincase

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.helloworld.ui.theme.HelloWorldTheme
 
 
@@ -23,7 +26,8 @@ class MainActivity : ComponentActivity() {
             HelloWorldTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Anna")
+                    HelloWorldWithImage(message = getString(R.string.hello_world_text), from = getString(
+                                            R.string.from_me))
                 }
             }
         }
@@ -31,17 +35,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Green) {
-        Text(text = "Hello World, my name is $name!", modifier = Modifier.padding(24.dp))
+fun HelloWorldWithText(message: String, from: String){
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun HelloWorldWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = "practice image layout",
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        HelloWorldWithText(message = message, from = from)
     }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun HelloWorldPreview() {
     HelloWorldTheme {
-        Greeting("anna")
+        HelloWorldWithImage(message = "Yo", from = "Anna")
     }
 }
